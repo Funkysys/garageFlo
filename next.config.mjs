8d40d1e1f05e458148737b3c8a65b5b7d67d1347
Funkysys <aadelbos@gmail.com>
2024-05-23 19:06:33 +0200

    search matters in deployment

--- next.config.mjs
+++ next.config.mjs
@@ -5,20 +5,41 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	images: {
-		remotePatterns: [
-			{
-				hostname: "www.logo-voiture.com",
-			},
-			{
-				hostname: "img.paruvendu.fr",
-			},
-			{ hostname: "images.unsplash.com" },
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://discophiles.vercel.app", // Set your origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "www.logo-voiture.com",
+      },
+      {
+        hostname: "img.paruvendu.fr",
+      },
+      { hostname: "images.unsplash.com" },
 
-			{ hostname: "raw.githubusercontent.com" },
-		],
-	},
+      { hostname: "raw.githubusercontent.com" },
+    ],
+  },
 };
 
 export default nextConfig;
